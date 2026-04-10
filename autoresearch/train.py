2446db6a89e31c55fb562a248c3b17ad6ed23e7f
HamzaEzzRa <hamzaezzaouirahali@gmail.com>
2026-04-09 23:20:42 -0400

    exp42: tighter gradient clipping max_norm=0.5 vs 1.0

--- autoresearch/train.py
+++ autoresearch/train.py
@@ -336,7 +336,9 @@ def train_predictor(
             optimizer.zero_grad(set_to_none=True)
             loss = msle_loss(predictor(inputs), targets)
             loss.backward()
-            torch.nn.utils.clip_grad_norm_(predictor.parameters(), max_norm=1.0)
+            torch.nn.utils.clip_grad_norm_(
+                predictor.parameters(), max_norm=0.5
+            )  # exp42: 0.5 vs 1.0
             optimizer.step()
 
             running_loss += loss.item()
